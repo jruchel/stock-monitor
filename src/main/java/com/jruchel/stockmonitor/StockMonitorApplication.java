c14fe57a3a697dd4eaef11f1a3c591e41683b909
Jakub Ruchel <jruchel254@gmail.com>
2021-12-29 20:59:06 +0100

    Created the project

---
+++ src/main/java/com/jruchel/stockmonitor/StockMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.jruchel.stockmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockMonitorApplication.class, args);
+    }
+
+}
